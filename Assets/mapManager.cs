5267941ebfc07a4b43100bee6ebc6d98e2036ac7
Julien Leveau <oursgroumy@gmail.com>
2018-09-21 08:32:26 +0200

    first commit

---
+++ Assets/mapManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class mapManager : MonoBehaviour {
+
+	public Color mouseOverColor;
+	Color originalColor;
+
+	Vector3Int selectedTilePosition;
+
+	public Tilemap tilemap;
+	// Use this for initialization
+	void Start () {
+		originalColor = tilemap.GetComponent<Tilemap>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void OnMouseOver()
+    {
+		tilemap.SetColor(selectedTilePosition, originalColor);
+		selectedTilePosition = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		tilemap.SetColor(selectedTilePosition, mouseOverColor);
+    }
+
+}
